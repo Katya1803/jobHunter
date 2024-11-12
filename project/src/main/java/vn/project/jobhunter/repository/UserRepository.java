a3acf4e65d88a07a56eb9a9381c8729f6c24e821
Katya1803 <vuvuworkplace@gmal.com>
2024-11-12 22:18:13 +0700

    Security config

---
+++ project/src/main/java/vn/project/jobhunter/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package vn.project.jobhunter.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import vn.project.jobhunter.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
